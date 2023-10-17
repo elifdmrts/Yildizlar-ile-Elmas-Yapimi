aacadd555746cf94021a6f44bfe230b14449d51b
Elif Demirtaş <elif_demirtas_@outlook.com>
2023-10-18 00:59:33 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Bir Sayı Giriniz :");
+        int n = scanner.nextInt();
+
+        int orta = n / 2 + 1;
+
+        // Üst kısmı çiz
+        for (int i = 1; i <= orta; i++) {
+            for (int j = 1; j <= orta - i; j++)
+                System.out.print(" ");
+
+            for (int k = 1; k <= 2 * i - 1; k++)
+                System.out.print("*");
+
+            System.out.println();
+        }
+
+        // Alt kısmı çiz
+        for (int i = orta - 1; i >= 1; i--) {
+            for (int j = 1; j <= orta - i; j++)
+                System.out.print(" ");
+
+            for (int k = 1; k <= 2 * i - 1; k++)
+                System.out.print("*");
+
+            System.out.println();
+        }
+    }
+}
